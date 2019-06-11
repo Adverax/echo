5ce57b6afb12c8d0a87a8e3b0ac4f76c2f1348c2
Adverax <adverax314@gmail.com>
2019-06-11 02:56:15 +0000

    Rewrited middleware cached

--- middleware/cached.go
+++ middleware/cached.go
@@ -12,49 +12,52 @@ func Cached(
 	e *echo.Echo,
 	class string,
 	dependencies func(c echo.Context) (map[string]string, error),
-	handler http.Handler,
 	duration time.Duration,
-) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var ctx echo.Context
-		if e.Complex {
-			ctx = echo.RequestContext(r)
-		} else {
-			ctx = e.AcquireContext()
-			defer e.ReleaseContext(ctx)
-			ctx.Reset(r, w)
+) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			var ctx echo.Context
+			if e.Complex {
+				ctx = echo.RequestContext(r)
+			} else {
+				ctx = e.AcquireContext()
+				defer e.ReleaseContext(ctx)
+				ctx.Reset(r, w)
+			}
+
+			deps, err := dependencies(ctx)
+			if err != nil {
+				ctx.Error(err)
+				return
+			}
+
+			var content []byte
+			err = e.Cacher.FetchData(
+				class,
+				deps,
+				&content,
+				func() (interface{}, error) {
+					rec := httptest.NewRecorder()
+					next.ServeHTTP(rec, r)
+
+					for k, v := range rec.Header() {
+						w.Header()[k] = v
+					}
+
+					w.WriteHeader(rec.Code)
+					return rec.Body.Bytes(), nil
+				},
+				duration,
+			)
+
+			if err != nil {
+				ctx.Error(err)
+				return
+			}
+
+			_, _ = w.Write(content)
 		}
 
-		deps, err := dependencies(ctx)
-		if err != nil {
-			ctx.Error(err)
-			return
-		}
-
-		var content []byte
-		err = e.Cacher.FetchData(
-			class,
-			deps,
-			&content,
-			func() (interface{}, error) {
-				rec := httptest.NewRecorder()
-				handler.ServeHTTP(rec, r)
-
-				for k, v := range rec.Header() {
-					w.Header()[k] = v
-				}
-
-				w.WriteHeader(rec.Code)
-				return rec.Body.Bytes(), nil
-			},
-			duration,
-		)
-
-		if err != nil {
-			ctx.Error(err)
-			return
-		}
-
-		_, _ = w.Write(content)
-	})
+		return http.HandlerFunc(fn)
+	}
 }
